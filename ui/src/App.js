47a4c79eed16dc3bdc8be17da632343ea6ce0f49
caizhenhong <1229542068@qq.com>
2021-12-13 14:50:42 +0800

    feat: 增加 button 和 input 基础组件

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,16 +1,8 @@
 import React, { Fragment } from 'react';
 import '@/assets/style/reset.less';
-import { Input, Button } from '@/components/Base';
 
 const App = () => {
-  return (
-    <Fragment>
-      <Input shape='round' placeholder='搜索'>
-        登录
-      </Input>
-      <Button type='primary'>退出登录</Button>
-    </Fragment>
-  );
+  return <Fragment></Fragment>;
 };
 
 export default App;
